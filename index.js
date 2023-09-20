d6889f804fc07c7a84ef5b83e7d327c06d62c90d
Hernandez olivera pedro <isc20350276@gmail.com>
2023-09-20 14:51:19 -0600

    query params

--- index.js
+++ index.js
@@ -1,23 +1,46 @@
 const express = require ('express')
+
 const app = express()
 
-// http://localhost:3000/hernandez
-app.get('/:user/:password', (req, res) =>{
-    const {user, password} = req.params
-    if (user == 'hernandez' && password == '123'){
-        res.json({msg:'inicio seccion exitoso'})
-        return
-    }
-    res. json({msg:'fallo contraseña invalida'})
+//http://localhost:3000/hernandez/123
+app.get('/', (req, response) =>{
+    response.json({msg: 'hello GET'})
 })
 
-app.put('/', (request, response) =>{
+app.get('/:user/:password', (req, response) =>{
+    const {user, password} = req.params
+    if (user == 'hernandez' && password =='123'){
+        response.json({msg: 'Inicio de sesion exitoso'})
+        return
+    }
+    response.json({msg: 'fallo en el usuario o contraseña'})
+})
+
+//http://localhost:3000/login?user=hernandez&password=123
+app.get('/login', (req, response) =>{
+    const {user, password} = req.query
+    if (!user || !password){
+        response.status(400).json({msg: 'you need to provide <user> and <password> parametros'})
+    }
+
+
+    if (user == 'tonatiuh' && password =='123'){
+        response.json({msg: 'Sign in succesfully'})
+        return
+    }
+    response.status({msg: 'wrong user or password'})
+})
+
+app.post('/', (req, response) =>{
+    response.json({msg: 'hello POST'})
+})
+app.put('/', (req, response) =>{
     response.json({msg:'Hola PUT'})
 })
-app.patch('/', (request, response) =>{
+app.patch('/', (req, response) =>{
     response.json({msg:'Hola PATCH'})
 })
-app.delete('/', (request, response) =>{
+app.delete('/', (req, response) =>{
     response.json({msg:'Hola DELETE'})
 })
 
